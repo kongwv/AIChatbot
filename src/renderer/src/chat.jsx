3a2b89f1335a516898839f7b93a5f3cf4b79d4cc
kongwv <116346840+kongwv@users.noreply.github.com>
2024-06-20 14:11:15 +0700

    update chat.jsx

--- src/renderer/src/chat.jsx
+++ src/renderer/src/chat.jsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
 import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
-import { MainContainer, ChatContainer, MessageList, Message, MessageInput } from '@chatscope/chat-ui-kit-react';
+import { MainContainer, ChatContainer, MessageList, Message, MessageInput, TypingIndicator, Avatar } from '@chatscope/chat-ui-kit-react';
 
 function Chat() {
   const ipcHandle = () => window.electron.ipcRenderer.send('ping');
+  const [typing, setTyping] = useState(false)
   const [messages, setMessages] = useState([
     {
       message: "Hello, how can I help you",
-      sender: "Chatbot",
+      sender: "chatbot",
       direction: "ingoing"
     }
   ]);
@@ -19,18 +20,28 @@ function Chat() {
       direction: "outgoing"
     }
     
-    const newMessages = [...messages, newMessage];
+    const newMessages = [...messages, newMessage]; // all the old messages, + the new message
 
+    // update our messages state
     setMessages(newMessages);
+    // set a typing indicator
+    setTyping(true);
+
   }
 
   return (
     <div style={{position:"relative", height:"700px", width:"550px"}}>
       <MainContainer>
         <ChatContainer>
-          <MessageList>
+          <MessageList typingIndicator = {typing ? <TypingIndicator content={"Typing"}/> : null}>
             {messages.map((message, i) => {
-              return <Message key={i} model={message}/>
+              if (message.sender == "chatbot") {
+                return (
+                  <Message key={i} model={message}>
+                    <Avatar name="chatbot" src="/src/images/chatbotpic.png"/>
+                  </Message>
+                )
+              } else return <Message key={i} model={message}/>
             })}
           </MessageList>
           <MessageInput placeholder="Type message here" attachButton={false} onSend={handleSend}/>
